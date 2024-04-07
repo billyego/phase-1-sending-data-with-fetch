bf9d93f17b06f52f9e130303439ddabc23fd7d0f
Bill <bill.kimtai@student.moringaschool.com>
2024-04-07 08:49:36 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email
+    };
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Unauthorized Access");
+            }
+            return response.json();
+        })
+        .then(data => {
+            const newId = data.id;
+            document.body.append(newId);
+            return data;
+        })
+        .catch(error => {
+            document.body.append(error.message);
+        });
+}
